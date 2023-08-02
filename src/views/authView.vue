1839898779d96a14a4fa3b16e0153acb4c688c03
maximo261199 <u201920341@upc.edu.pe>
2023-08-02 10:16:11 -0500

    base para todas las autenticaciones

---
+++ src/views/authView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model="email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model="password">
+  </div>
+ 
+  <button type="submit" class="btn btn-primary mt-2">Submit</button>
+
+</form>
+
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const email = ref("");
+const password = ref("");
+</script>
+
+<style lang="scss" scoped>
+
+</style>
